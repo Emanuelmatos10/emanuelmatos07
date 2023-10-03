16d4ff623da7b5b486d607accdd8c77879cc1445
Emanuelmatos10 <105299869+Emanuelmatos10@users.noreply.github.com>
2023-10-03 09:05:41 -0300

    .

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
    }
 const ListaDeTlecas = document.querySelectorAll('.tecla');
+
+
+let contador = 0; 
+
+//enquanto
+while (contador < ListaDeTlecas.length) {
+
+ListaDeTlecas[0].onclick = tocaSomPom;
+
+ contador = contador = 1;
+
+ console.log (contador);
+ }
