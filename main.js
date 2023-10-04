c9a46bde609893dc3487408210b6015415577f9c
Emanuelmatos10 <105299869+Emanuelmatos10@users.noreply.github.com>
2023-10-04 14:32:32 -0300

    .

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function tocaSomPom () {
-
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio) {
+     document.querySelector(idElementoAudio).play();
    }
-const ListaDeTlecas = document.querySelectorAll('.tecla');
 
+const ListaDeTlecas = document.querySelectorAll('.tecla');
 
 let contador = 0; 
 
-//enquanto
-while (contador < ListaDeTlecas.length) {
+//para
+    for (let contador = 0; contador < ListaDeTlecas.length; contador ++){
 
-ListaDeTlecas[0].onclick = tocaSomPom;
+    const tecla = ListaDeTlecas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
- contador = contador = 1;
+    tecla.onclick = function (){
+    tocaSom(idAudio);
+    }
 
- console.log (contador);
- }
+}
